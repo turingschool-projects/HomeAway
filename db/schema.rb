8e2aae09d31e98d73c4767842ca6ef0e409179ac
Greg Narajka <gregnar@gmail.com>
2014-11-19 10:59:04 -0700

    working on sidebar shit

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20141119003251) do
     t.datetime "updated_at"
   end
 
+  create_table "examples", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "item_categories", force: true do |t|
     t.integer "item_id"
     t.integer "category_id"
