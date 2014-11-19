29fb4f763bb0dbce9cb5f1381a7add26fb2fd36a
ChasevanHekken <cvh1717@gmail.com>
2014-11-19 10:58:34 -0700

    finished items controller spec

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,15 +4,28 @@ class ItemsController < ApplicationController
   end
 
   def show
-    
+    @item = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price)
   end
 end
