e5f7507a2c6e70963171f3fc13f05fa5b0a52379
Konr Larson <larsonkonr@gmail.com>
2014-11-19 09:32:23 -0700

    navbar affix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,11 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+    $(".navbar").affix({
+        offset: {
+            top: 125
+     	}
+    });
+});
