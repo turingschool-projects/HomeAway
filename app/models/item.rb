c4d1e9cd078ab2edc1d3caa0b70315acb18ab41c
ChasevanHekken <cvh1717@gmail.com>
2014-11-19 18:56:27 -0700

    paperclip edit menu item photo

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,12 @@ class Item < ActiveRecord::Base
   has_many :categories, through: :item_categories
   has_many :order_items
   has_many :orders, through: :order_items
+
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
