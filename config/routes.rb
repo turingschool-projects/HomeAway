8803f6d04fd5f963eaff7d660e98f8a9780efb7f
Viki Harrod <victoria.harrod@gmail.com>
2014-11-20 11:13:19 -0700

    add menu link to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :items
 
+  get 'menu', to: 'items#index', as: :menu
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
