1cf1d9a20c1b76e78338ad314046b563e9325a69
ChasevanHekken <cvh1717@gmail.com>
2014-11-19 19:19:41 -0700

    paperclip right size

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ActiveRecord::Base
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
-  }
+    }
 
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
