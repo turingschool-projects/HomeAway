6c4894ff5a43d0389589d3bcf516870cddb417b5
Viki Harrod <victoria.harrod@gmail.com>
2014-11-20 18:52:42 -0700

    add conditional to _navbar to check login/logout state, add feature spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
 
   def find_resource
     class_name = params[:controller].singularize
     klass = class_name.camelize.constantize
     self.instance_variable_set "@" + class_name, klass.find(params[:id])
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
