97253a795d93d468dcace88318d3bb82a2e35ca3
Viki Harrod <victoria.harrod@gmail.com>
2014-11-24 16:44:17 -0700

    add modify link

--- app/controllers/admin/base_admin_controller.rb
+++ app/controllers/admin/base_admin_controller.rb
@@ -2,7 +2,9 @@ class Admin::BaseAdminController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :require_admin
   helper_method :current_user
-  before_action :render_layout
+
+  layout 'admin'
+
   protected
 
   def current_user
@@ -14,9 +16,5 @@ class Admin::BaseAdminController < ActionController::Base
       flash[:notice] = "Unauthorized"
       redirect_to root_path
     end
-  end
-
-  def render_layout
-    render 'layouts/application.html.erb'
   end
 end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -17,7 +17,7 @@ class Admin::ItemsController < Admin::BaseAdminController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to items_path
+      redirect_to admin_items_path
     else
       render :new
     end
@@ -25,7 +25,7 @@ class Admin::ItemsController < Admin::BaseAdminController
 
   def update
     @item.update(item_params)
-    redirect_to items_path
+    redirect_to admin_items_path
   end
 
   def edit
