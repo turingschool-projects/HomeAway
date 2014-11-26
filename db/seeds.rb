fd9593fee825bc51a84e81648f9c7e3fb884999d
Viki Harrod <victoria.harrod@gmail.com>
2014-11-26 10:48:01 -0700

    re-organized seed file with new names for items

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,9 @@ categories = Category.create([
   { name: "Appetizers" },
   { name: "Burgers" },
   { name: "Entrees" },
-  { name: "Desserts" },
-  { name: "Local Game" }])
+  { name: "Local Game" },
+  { name: "Desserts" }
+  ])
 
 users = User.create!([
   { name: "Rachel Warbelow",
@@ -35,7 +36,25 @@ users = User.create!([
     admin: true }
   ])
 
-foods = Item.create([
+appetizers = Item.create([
+  { title: "Deviled Quail Eggs",
+    description: "Free range quail eggs, perfect for sharing",
+    price: 5.50 },
+  { title: "Bison Chili Cheese Fries",
+    description: "Just like your regular chili cheese fries, but bison",
+    price: 5.50 },
+  { title: "Fried Green Beans",
+    description: "Home grown green beans battered and fried up nice and crispy",
+    price: 5.50 },
+  { title: "Tomato Cheese Sandwich",
+    description: "Fresh tomatoes, spinach and cheese top a slice of fresh mountain bread",
+    price: 5.50 },
+  { title: "Nachos",
+    description: "Free range nachos with a bunch of stuff",
+    price: 5.50 }
+    ])
+
+burgers = Item.create([
   { title: "Bison Burger",
     description: "Free range bison meat with cheddar cheese grilled onions and mushrooms topped with a homemade pickle",
     price: 9.50 },
@@ -50,106 +69,83 @@ foods = Item.create([
     price: 8.75 },
   { title: "Bear Burger",
     description: "Free range bear meat with swiss cheese peppers and grilled onions topped with a homemade pickle",
-    price: 9.75 },
+    price: 9.75 }
+    ])
 
-
-  { title: "Quail Eggs1",
-    description: "Free range quail eggs with a bunch of stuff",
-    price: 5.50 },
-  { title: "Quail Eggs2",
-    description: "Free range quail eggs with a bunch of stuff",
-    price: 5.50 },
-  { title: "Quail Eggs3",
-    description: "Free range quail eggs with a bunch of stuff",
-    price: 5.50 },
-  { title: "Quail Eggs4",
-    description: "Free range quail eggs with a bunch of stuff",
-    price: 5.50 },
-  { title: "Quail Eggs5",
-    description: "Free range quail eggs with a bunch of stuff",
-    price: 5.50 },
-
-
-  { title: "Wild Boar Tenderloin1",
+entrees = Item.create([
+  { title: "Wild Boar Tenderloin",
     description: "Free range wild boar with tusks and stuff",
     price: 14.50 },
-  { title: "Wild Boar Tenderloin2",
-    description: "Free range wild boar with tusks and stuff",
-    price: 14.50 },
-  { title: "Wild Boar Tenderloin3",
-    description: "Free range wild boar with tusks and stuff",
-    price: 14.50 },
-  { title: "Wild Boar Tenderloin4",
-    description: "Free range wild boar with tusks and stuff",
-    price: 14.50 },
-  { title: "Wild Boar Tenderloin5",
-    description: "Free range wild boar with tusks and stuff",
-    price: 14.50 },
-
-
-  { title: "Chocolate Cake1",
-    description: "Free range cocoa beans and cane sugar",
-    price: 4.50 },
-  { title: "Chocolate Cake2",
-    description: "Free range cocoa beans and cane sugar",
-    price: 4.50 },
-  { title: "Chocolate Cake3",
-    description: "Free range cocoa beans and cane sugar",
-    price: 4.50 },
-  { title: "Chocolate Cake4",
-    description: "Free range cocoa beans and cane sugar",
-    price: 4.50 },
-  { title: "Chocolate Cake5",
-    description: "Free range cocoa beans and cane sugar",
-    price: 4.50 },
-
-
-
-  { title: "Mountain Lion1",
+  { title: "Mountain Lion",
     description: "Free range mountain lion steak with home grown french fries",
     price: 13.50 },
-  { title: "Mountain Lion2",
+  { title: "Shepherd's Pie",
+    description: "Free range mountain lamb baked in vegetables and potatoes",
+    price: 12.50 },
+  { title: "Mountain Chicken Pot Pie",
+    description: "Free range mountain chicken baked in a pie with veggies",
+    price: 11.50}
+    ])
+
+local_game = Item.create([
+  { title: "Mountain Lion Steak",
     description: "Free range mountain lion steak with home grown french fries",
     price: 13.50 },
-  { title: "Mountain Lion3",
-    description: "Free range mountain lion steak with home grown french fries",
-    price: 13.50 },
-  { title: "Mountain Lion4",
-    description: "Free range mountain lion steak with home grown french fries",
-    price: 13.50 },
-  { title: "Mountain Lion5",
-    description: "Free range mountain lion steak with home grown french fries",
+  { title: "Mountain Lion Stew",
+    description: "Free range mountain lion stewed with carrots and potatoes",
     price: 13.50 }
+    ])
 
-])
+desserts = Item.create([
+  { title: "Chocolate Cake",
+    description: "Free range cocoa beans and cane sugar",
+    price: 4.50 },
+  { title: "Chocolate Cake Sundae",
+    description: "Free range cocoa beans and cane sugar, plus ice cream",
+    price: 4.50 },
+  { title: "Apple Pie",
+    description: "Home grown apples baked in a delicious pie",
+    price: 4.50 },
+  { title: "Apple Pie A La Mode",
+    description: "Home grown apples baked in a delicious pie, plus ice cream",
+    price: 4.50 }
+  ])
 
-item_categories = ItemCategory.create([
-  { item_id: 1, category_id: 2 },
-  { item_id: 1, category_id: 5 },
-  { item_id: 2, category_id: 2 },
-  { item_id: 3, category_id: 2 },
-  { item_id: 4, category_id: 2 },
-  { item_id: 5, category_id: 2 },
-  { item_id: 6, category_id: 1 },
-  { item_id: 7, category_id: 1 },
-  { item_id: 8, category_id: 1 },
-  { item_id: 9, category_id: 1 },
-  { item_id: 10, category_id: 1 },
+appetizer_items = ItemCategory.create([
+  { item_id: 1, category_id: 1 },
+  { item_id: 2, category_id: 1 },
+  { item_id: 3, category_id: 1 },
+  { item_id: 4, category_id: 1 },
+  { item_id: 5, category_id: 1 }
+  ])
+
+burger_items = ItemCategory.create([
+  { item_id: 6, category_id: 2 },
+  { item_id: 7, category_id: 2 },
+  { item_id: 8, category_id: 2 },
+  { item_id: 9, category_id: 2 },
+  { item_id: 10, category_id: 2 }
+  ])
+
+entree_items = ItemCategory.create([
   { item_id: 11, category_id: 3 },
-  { item_id: 11, category_id: 5 },
   { item_id: 12, category_id: 3 },
   { item_id: 13, category_id: 3 },
-  { item_id: 14, category_id: 3 },
-  { item_id: 15, category_id: 3 },
-  { item_id: 16, category_id: 4 },
-  { item_id: 17, category_id: 4 },
-  { item_id: 18, category_id: 4 },
-  { item_id: 19, category_id: 4 },
-  { item_id: 20, category_id: 4 },
-  { item_id: 21, category_id: 5 },
-  { item_id: 22, category_id: 5 },
-  { item_id: 23, category_id: 5 },
-  { item_id: 24, category_id: 5 },
-  { item_id: 25, category_id: 5 }
+  { item_id: 14, category_id: 3 }
+  ])
 
+local_game_items = ItemCategory.create([
+  { item_id: 15, category_id: 4 },
+  { item_id: 16, category_id: 4 },
+  { item_id: 6, category_id: 4 },
+  { item_id: 7, category_id: 4 },
+  { item_id: 11, category_id: 4 }
+  ])
+
+dessert_items = ItemCategory.create([
+  { item_id: 17, category_id: 5 },
+  { item_id: 18, category_id: 5 },
+  { item_id: 19, category_id: 5 },
+  { item_id: 20, category_id: 5 },
+  { item_id: 21, category_id: 5 }
 ])
