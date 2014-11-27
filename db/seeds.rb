82dbf03055d4587b7133177b25174ca35d477485
Viki Harrod <victoria.harrod@gmail.com>
2014-11-26 17:37:15 -0700

    add images to seed file for all items except burgers

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,20 +43,26 @@ appetizers = Item.create([
   { title: "Bison Chili Cheese Fries",
     description: "Just like your regular chili cheese fries, but bison",
     price: 5.50 },
-  { title: "Fried Green Beans",
-    description: "Home grown green beans battered and fried up nice and crispy",
+  { title: "Tomato Bruschetta",
+    description: "Home grown tomatoes, herbs and onions on sliced baguette",
     price: 5.50 },
   { title: "Tomato Cheese Sandwich",
     description: "Fresh tomatoes, spinach and cheese top a slice of fresh mountain bread",
     price: 5.50 },
-  { title: "Beautiful Soup",
-    description: "Lovely green soup with a creamy star on top",
+  { title: "Lucky Soup",
+    description: "Creamy cheese soup topped with a four-leaf clover",
     price: 5.50 }
     ])
 appetizers[0].image = File.open("#{Rails.root}/app/assets/images/deviled-quail-eggs.png")
 appetizers[0].save
+appetizers[1].image = File.open("#{Rails.root}/app/assets/images/chili-cheese-fries.png")
+appetizers[1].save
+appetizers[2].image = File.open("#{Rails.root}/app/assets/images/tomato-bruschetta.png")
+appetizers[2].save
 appetizers[3].image = File.open("#{Rails.root}/app/assets/images/tomato-veg-cheese-sandwich.png")
 appetizers[3].save
+appetizers[4].image = File.open("#{Rails.root}/app/assets/images/lucky-soup.jpg")
+appetizers[4].save
 
 burgers = Item.create([
   { title: "Bison Burger",
@@ -90,21 +96,28 @@ entrees = Item.create([
     description: "Free range mountain chicken baked in a pie with veggies",
     price: 11.50}
     ])
+entrees[0].image = File.open("#{Rails.root}/app/assets/images/pork-tenderloin.png")
+entrees[0].save
+entrees[1].image = File.open("#{Rails.root}/app/assets/images/mountain-lion-steak.jpg")
+entrees[1].save
+entrees[2].image = File.open("#{Rails.root}/app/assets/images/shepherds-pie.jpg")
+entrees[2].save
+entrees[3].image = File.open("#{Rails.root}/app/assets/images/chicken-pot-pie.jpg")
+entrees[3].save
 
 local_game = Item.create([
-  { title: "Mountain Lion Steak",
-    description: "Free range mountain lion steak with home grown french fries",
-    price: 13.50 },
   { title: "Mountain Lion Stew",
     description: "Free range mountain lion stewed with carrots and potatoes",
     price: 13.50 }
     ])
+local_game[0].image = File.open("#{Rails.root}/app/assets/images/mountain-lion-stew.jpg")
+local_game[0].save
 
 desserts = Item.create([
   { title: "Chocolate Cake",
     description: "Free range cocoa beans and cane sugar",
     price: 4.50 },
-  { title: "Chocolate Cake Sundae",
+  { title: "Brownie Sundae",
     description: "Free range cocoa beans and cane sugar, plus ice cream",
     price: 4.50 },
   { title: "Apple Pie",
@@ -114,6 +127,13 @@ desserts = Item.create([
     description: "Home grown apples baked in a delicious pie, plus ice cream",
     price: 4.50 }
   ])
+desserts[0].image = File.open("#{Rails.root}/app/assets/images/chocolate-cake.jpg")
+desserts[0].save
+desserts[1].image = File.open("#{Rails.root}/app/assets/images/brownie-sundae.jpg")
+desserts[1].save
+desserts[2].image = File.open("#{Rails.root}/app/assets/images/apple-pie.jpg")
+desserts[2].save
+desserts[3].image = File.open("#{Rails.root}/app/assets/images/apple-pie-a-la-mode.jpg")
 
 appetizer_items = ItemCategory.create([
   { item_id: 1, category_id: 1 },
@@ -140,16 +160,15 @@ entree_items = ItemCategory.create([
 
 local_game_items = ItemCategory.create([
   { item_id: 15, category_id: 4 },
-  { item_id: 16, category_id: 4 },
+  { item_id: 2, category_id: 4 },
   { item_id: 6, category_id: 4 },
-  { item_id: 7, category_id: 4 },
-  { item_id: 11, category_id: 4 }
+  { item_id: 10, category_id: 4 }
   ])
 
 dessert_items = ItemCategory.create([
+  { item_id: 16, category_id: 5 },
   { item_id: 17, category_id: 5 },
   { item_id: 18, category_id: 5 },
   { item_id: 19, category_id: 5 },
-  { item_id: 20, category_id: 5 },
-  { item_id: 21, category_id: 5 }
+  { item_id: 20, category_id: 5 }
 ])
