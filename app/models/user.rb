cae40c07315095e28bf8bdd5c816b406787e2190
Viki Harrod <victoria.harrod@gmail.com>
2014-11-26 22:55:16 -0700

    add user validations and some edge case specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :orders
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email_address, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :name, presence: true
+  validates :display_name, length: {minimum: 2, maximum: 32}, allow_blank: true
 end
