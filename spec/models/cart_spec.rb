4f1606843a76c577d5c11de165973da6639d615f
Viki Harrod <victoria.harrod@gmail.com>
2014-11-29 15:08:54 -0700

    uh, here's the cart spec and total column migrations :p

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe Cart do
+  let(:item1) {Item.create!(title: "Test item", description: "test", price: 1.0)}
+  let(:item2) {Item.create!(title: "Another test item", description: "other test", price: 2.0)}
+  let(:session_hash) do
+    {item1.id.to_s => 2, item2.id.to_s => 1}
+  end
+
+  subject(:cart) {Cart.new(session_hash)}
+  it "loads a correct hash" do
+    expect(cart.to_h).to eq({item1.id => 2, item2.id => 1})
+  end
+
+  it "can count number of particular item" do
+    expect(cart.count_of(item1)).to eq(2)
+  end
+
+  it "can count total items" do
+    expect(cart.total_items).to eq(3)
+  end
+
+  it "can calculate total cost" do
+    expect(cart.total_cost).to eq(4.0)
+  end
+
+  it "can add more items" do
+    cart.add_item(item1)
+    expect(cart.total_cost).to eq(5.0)
+    expect(cart.total_items).to eq(4)
+  end
+end
