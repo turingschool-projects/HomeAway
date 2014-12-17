cd5e51df74337f365b61ce7a772ded416c340bd7
Alex <96dalex@gmail.com>
2014-12-16 18:27:05 -0700

    mostly finished the first iteration feature specs

---
+++ spec/feature/trips_feature_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+# delete the x here to unskip when ready
+xdescribe "booked trips", type: :feature do
+  it "can view a page with reservation details" do
+    pending "Implement me"
+    # this needs a status and property details
+  end
+
+  it "can view a page with upcoming and past reservations" do
+    pending "Implement me"
+  end
+end
