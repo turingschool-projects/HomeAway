7337789a0a805c786c26590c18f24574e5a0f5ad
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-12-17 17:08:55 -0700

    Adds images to seed file

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,13 @@
 class Photo < ActiveRecord::Base
+  has_attached_file :image,
+    styles: {
+    hero: '1280x768',
+    thumb: '100x100>',
+    medium: '235x135',
+  },
+  default_url: 'ext_balloon_1.jpg'
+
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
   belongs_to :property
 end
