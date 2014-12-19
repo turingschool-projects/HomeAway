e08932a3c57d09ff15919dd88ed9feba34288ac6
Viki Harrod <victoria.harrod@gmail.com>
2014-12-18 23:03:46 -0700

    really not sure how to test a photo...

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Photo, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "must belong to a property" do
+    photo = Photo.create(image: File.open("app/assets/images/ext_apt_1.jpg"))
+    expect(photo).to_not be_valid
+
+    property =
+    photo = Photo.create(image: File.open("app/assets/images/ext_apt_1.jpg"), property_id: 1)
+    expect(photo).to be_valid
+  end
+
+  it "has an image" do
+    photo = Photo.create(image: File.open("app/assets/images/ext_apt_1.jpg"), property_id: 1)
+    expect(photo.image).to_not be_nil
+  end
 end
