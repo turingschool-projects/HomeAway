e89c84ac972828a48fbdf66a8f3765f8e65664a9
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-12-19 17:14:18 -0700

    Removes unpushed migration that i was going to use for adding lat and long to the address, but we don't need that anymore

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219201035) do
+ActiveRecord::Schema.define(version: 20141218223212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,6 @@ ActiveRecord::Schema.define(version: 20141219201035) do
     t.string   "country",    default: "USA"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.float    "latitude"
-    t.float    "longitude"
   end
 
   create_table "categories", force: true do |t|
