16cb5d6fdd945bc5c22747749f52555e4a22572b
Viki Harrod <victoria.harrod@gmail.com>
2014-12-27 19:27:25 -0700

    unskip and implement

--- spec/feature/trips_feature_spec.rb
+++ spec/feature/trips_feature_spec.rb
@@ -1,13 +1,56 @@
 require "rails_helper"
 
-# delete the x here to unskip when ready
-xdescribe "booked trips", type: :feature do
+describe "booked trips", type: :feature do
+  before(:each) do
+    User.create! name: "Viki Harrod",
+                 email_address: "viki@example.com",
+                 password: "password",
+                 password_confirmation: "password"
+    Category.create! name: "Awesome Place"
+    Address.create! line_1: "213 Some St",
+                    city: "Denver",
+                    state: "CO",
+                    zip: "80203"
+    Property.create! title: "My Cool Home", description: "cool description",
+                     occupancy: 4, price: 666,
+                     bathroom_private: false,
+                     category: Category.last,
+                     address: Address.last
+    Reservation.create! user: User.last,
+                        property: Property.last,
+                        start_date: Date.current.advance(days: 20),
+                        end_date: Date.current.advance(days: 25)
+  end
   it "can view a page with reservation details" do
-    pending "Implement me"
-    # this needs a status and property details
+    visit root_path
+    find_link("Log In").click
+    fill_in "email_address", with: "viki@example.com"
+    fill_in "password", with: "password"
+    find_button("Login").click
+    visit reservation_path(Reservation.last)
+    expect(page).to have_content("My Cool Home")
+    expect(page).to have_content("5 days")
+    expect(page).to have_content("$33.30")
   end
 
   it "can view a page with upcoming and past reservations" do
-    pending "Implement me"
+    visit root_path
+    find_link("Log In").click
+    fill_in "email_address", with: "viki@example.com"
+    fill_in "password", with: "password"
+    find_button("Login").click
+    visit reservations_path
+
+    expect(page).to have_content("Upcoming")
+    expect(page).to have_css(".upcoming")
+    within(".upcoming") do
+      expect(page).to have_content("My Cool Home")
+      expect(page).to have_content("$33.30")
+      expect(page).to have_content("Denver")
+      expect(page).to have_content("pending")
+    end
+
+    expect(page).to have_content("Completed")
+    expect(page).to_not have_css(".completed")
   end
 end
