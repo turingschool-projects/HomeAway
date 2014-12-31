09d39d47671b8d0d3283342d8b78115d8f649759
Viki Harrod <victoria.harrod@gmail.com>
2014-12-31 00:24:08 -0700

    add welcome email and fix syntax errors

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,13 @@
 class UserMailer < ActionMailer::Base
   default from: "no-reply@mighty-escarpment-8434.herokuapp.com"
 
+  def welcome_email(user)
+    mail(to: user.email_address,
+    subject: 'Welcome to TravelHome!') do |format|
+      format.text { render text: "Dear #{user.name},\n\nWelcome to TravelHome! Please visit http://mighty-escarpment-8434.herokuapp.com#{edit_user_path(user)} and add your address to complete the sign-up process.\n\nOnce you're done, feel free to browse our locations at http://mighty-escarpment-8434.herokuapp.com#{properties_path} to find your next dream vacation destination. Happy Travels!\n\nBest Regards,\n\n--TravelHome" }
+    end
+  end
+
   def confirmation_email(reservation)
     mail(to: reservation.user.email_address,
     subject: 'Reservation Approved!') do |format|
@@ -11,13 +18,14 @@ class UserMailer < ActionMailer::Base
   def cancellation_email(reservation)
     mail(to: reservation.property.user.email_address,
     subject: 'Reservation Cancellation') do |format|
-      format.text { render text: "Dear #{reservation.property.user.name},\n\nThe previously requested reservation by #{reservation.user.name} for #{reservation.property.title} has been cancelled. We hope all is well.\n\nBest Regards,\n\n--TravelHome"}
+      format.text { render text: "Dear #{reservation.property.user.name},\n\nThe previously requested reservation by #{reservation.user.name} for #{reservation.property.title} has been cancelled. We hope all is well.\n\nBest Regards,\n\n--TravelHome" }
     end
   end
 
   def denial_email(reservation)
     mail(to: reservation.user.email_address,
     subject: 'Reservation Declined') do |format|
-      format.text { render text: "Dear #{reservation.user.name},\n\nWe regret to inform you that the reservation you requested from #{reservation.property.user.name} for #{reservation.property.title} for the dates of #{reservation.pretty_start_date} to #{reservation.pretty_end_date} has been denied. Sorry about that!\n\nBest Regards,\n\n--TravelHome"}
+      format.text { render text: "Dear #{reservation.user.name},\n\nWe regret to inform you that the reservation you requested from #{reservation.property.user.name} for #{reservation.property.title} for the dates of #{reservation.pretty_start_date} to #{reservation.pretty_end_date} has been denied. Sorry about that!\n\nBest Regards,\n\n--TravelHome" }
+    end
   end
 end
