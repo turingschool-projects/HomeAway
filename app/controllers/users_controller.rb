a65be3419a0e51e4d585af96fbd0b9521745181b
Viki Harrod <victoria.harrod@gmail.com>
2014-12-31 09:09:51 -0700

    add welcome email to user controller on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      UserMailer.welcome_email(@user).deliver
       redirect_to :back
     else
       render :new
