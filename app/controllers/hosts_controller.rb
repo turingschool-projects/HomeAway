6c17c659e638be61ecace30b04d3ed0d6c636a9d
Alex <96dalex@gmail.com>
2014-12-31 10:05:58 -0700

    these features didnt know what was coming being on my hitlist

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,8 +1,12 @@
 class HostsController < ApplicationController
+  helper_method :is_the_host?
   def show
     user = User.hosts.find_by(host_slug: params[:slug]) || not_found
     @properties = Property.active.for_user(user.id)
+    @is_the_host = user == current_user
   end
 
-
+  def is_the_host?
+    @is_the_host
+  end
 end
