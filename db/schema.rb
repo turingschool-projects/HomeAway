d33b106d87e17334e565fb20495de76d104ae845
Sam Kuhlmann <samkuhlmann@gmail.com>
2015-01-03 19:21:03 -0700

    Styles reservation index

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,53 @@ ActiveRecord::Schema.define(version: 20150103022224) do
     t.datetime "updated_at"
   end
 
+  create_table "categories_items", id: false, force: true do |t|
+    t.integer "category_id", null: false
+    t.integer "item_id",     null: false
+  end
+
+  add_index "categories_items", ["category_id", "item_id"], name: "index_categories_items_on_category_id_and_item_id", using: :btree
+  add_index "categories_items", ["item_id", "category_id"], name: "index_categories_items_on_item_id_and_category_id", using: :btree
+
+  create_table "coordinates", force: true do |t|
+    t.string   "latitude"
+    t.string   "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "items", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
+    t.boolean  "active",             default: true
+    t.integer  "supplier_id"
+  end
+
+  create_table "items_orders", id: false, force: true do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id",  null: false
+    t.integer "quantity"
+  end
+
+  add_index "items_orders", ["item_id", "order_id"], name: "index_items_orders_on_item_id_and_order_id", using: :btree
+  add_index "items_orders", ["order_id", "item_id"], name: "index_items_orders_on_order_id_and_item_id", using: :btree
+
+  create_table "orders", force: true do |t|
+    t.boolean  "delivery"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.boolean  "pending"
+    t.integer  "coordinate_id"
+  end
+
   create_table "photos", force: true do |t|
     t.string   "image_file_name"
     t.string   "image_content_type"
@@ -71,6 +118,23 @@ ActiveRecord::Schema.define(version: 20150103022224) do
     t.integer  "property_id"
     t.date     "start_date"
     t.date     "end_date"
+  end
+
+  create_table "supplier_users", id: false, force: true do |t|
+    t.integer "user_id",     null: false
+    t.integer "supplier_id", null: false
+  end
+
+  create_table "suppliers", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "fax"
+    t.string   "description"
+    t.integer  "address_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "slug"
   end
 
   create_table "users", force: true do |t|
