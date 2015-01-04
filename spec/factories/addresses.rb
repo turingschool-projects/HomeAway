13f498d211a70732acbd9a88010214b4131931e0
Viki Harrod <victoria.harrod@gmail.com>
2015-01-04 16:05:35 -0700

    addresses are now sorta real (city/state/zip are, street is real enough to get a marker)

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,8 +1,24 @@
+require 'csv'
+
 FactoryGirl.define do
   factory :address do
-    line_1 "123 Some St"
-    city "Denver"
-    state "CO"
-    zip "80203"
+    sequence(:line_1) { |n| AddressReader.csv_row(n)[:line_1] }
+    sequence(:city) { |n| AddressReader.csv_row(n)[:city] }
+    sequence(:state) { |n| AddressReader.csv_row(n)[:state] }
+    sequence(:zip) { |n| AddressReader.csv_row(n)[:zip] }
+  end
+end
+
+class AddressReader
+  include Singleton
+
+  def self.csv_row(n)
+    instance.csv_row(n)
+  end
+
+  def csv_row(n)
+    @addresses ||= CSV.read(Rails.root.join('spec', 'fixtures', 'addresses.csv'), headers: true, header_converters: :symbol)
+
+    @addresses[n]
   end
 end
