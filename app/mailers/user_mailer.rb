6ea633281b765557372ee18df389b4cce3966836
Viki Harrod <victoria.harrod@gmail.com>
2015-01-07 11:13:41 -0700

    pull welcome email into views

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: "no-reply@travel-home.herokuapp.com"
 
   def welcome_email(user)
+    @user = user
     mail(to: user.email_address,
-    subject: 'Welcome to TravelHome!') do |format|
-      format.text { render text: "Dear #{user.name},\n\nWelcome to TravelHome! Please visit http://travel-home.herokuapp.com#{edit_user_path(user)} and add your address to complete the sign-up process.\n\nOnce you're done, feel free to browse our locations at http://travel-home.herokuapp.com#{properties_path} to find your next dream vacation destination. Happy Travels!\n\nBest Regards,\n\n--TravelHome" }
-    end
+    subject: 'Welcome to TravelHome!')
   end
 end
