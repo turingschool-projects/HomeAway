f8a23630d872fb47b66af06de19d01e3fa041ec3
Viki Harrod <victoria.harrod@gmail.com>
2015-01-07 11:34:59 -0700

    pull out denial_email into views

--- app/mailers/traveler_mailer.rb
+++ app/mailers/traveler_mailer.rb
@@ -14,9 +14,8 @@ class TravelerMailer < ActionMailer::Base
   end
 
   def denial_email(reservation)
+    @reservation = reservation
     mail(to: reservation.user.email_address,
-    subject: 'Reservation Declined') do |format|
-      format.text { render text: "Dear #{reservation.user.name},\n\nWe regret to inform you that the reservation you requested for #{reservation.property.title} for the dates of #{reservation.pretty_start_date} to #{reservation.pretty_end_date} has been denied. You will not be charged for this reservation. Sorry about that!\n\nBest Regards,\n\n--TravelHome" }
-    end
+    subject: 'Reservation Declined')
   end
 end
