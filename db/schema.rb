cdfaf3e9b88259950c8bfd1c22a6919e0096673b
Miriam Moser <mirjoy.moser@gmail.com>
2015-05-22 21:48:28 -0600

    added favorites table, added heart icon to properties show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105201406) do
+ActiveRecord::Schema.define(version: 20150523032805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20150105201406) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "favorites", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "property_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
